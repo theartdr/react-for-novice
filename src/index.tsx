9a26892cb5769d47355a6e7fdb4d080e3002b906
theartdr <129827570+theartdr@users.noreply.github.com>
2023-06-08 22:57:13 +0900

    Add files via upload
    setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ThemeProvider } from 'styled-components';
-import { darkTheme, lightTheme } from './theme';
+import { theme } from './theme';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
   <React.StrictMode>
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
   </React.StrictMode>
-  ,document.getElementById("root")
 );
